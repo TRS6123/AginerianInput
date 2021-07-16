ec1ef3241752e671c54f6c0bf72dc1c9a7c3df77
Andy Raut <mcrautscout@gmail.com>
2021-07-16 00:58:31 -0400

    Removed uneccessary files

--- Runtime/IInput.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine.InputSystem;
-
-namespace Aginerian.Input
-{
-    public interface IInput<T> where T : struct
-    {
-        void SetInput(InputAction.CallbackContext ctx, int index);
-    }
-}
